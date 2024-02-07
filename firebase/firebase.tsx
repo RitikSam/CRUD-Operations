6a1727103d4558458f6861904fab9c63420153ea
RitikSam <136999383+RitikSam@users.noreply.github.com>
2024-02-07 16:29:54 +0530

    Firebase

--- firebase/firebase.tsx
+++ firebase/firebase.tsx
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; 
 
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:  process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  
-
+  apiKey: "AIzaSyCViDi8bQ9qCW-EqQLehuTZkZGo1J10b1w",
+  authDomain: "crud-app-5457b.firebaseapp.com",
+  projectId: "crud-app-5457b",
+  storageBucket: "crud-app-5457b.appspot.com",
+  messagingSenderId: "567464796458",
+  appId: "1:567464796458:web:5ecab96f4527e958e1be53",
+  measurementId: "G-1K5KWDJZ5B"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+// export const auth = getAuth(app);
 export const db = getFirestore(app);
